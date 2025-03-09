1f428f0c415efda4192affadf7cbbcf511b31278
jeantigreros <jtigreros92@gmail.com>
2025-03-09 12:55:19 -0500

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,47 +4,43 @@ end
 
 starship init fish | source
 
-# Git configuration aliases
-
-function dotfiles --description "dotfiles commands with bare git repo"
-    GIT_WORK_TREE=~ GIT_DIR=~/.cfg/ $argv
-end
-
-function config
-    /usr/bin/git --git-dir=/home/j/.cfg --work-tree=/home/j $argv
-end
-
-function ccs 
+function ccs
     config status
 end
 
-function ccm 
+function ccm
     config commit -m "$argv"
 end
 
-function ccp 
+function ccp
     config push
 end
 
-function cca 
+function cca
     config add $argv
 end
 
-function ggs 
+function ggs
     git status
 end
 
-function ggm 
+function ggm
     git commit -m "$argv"
 end
 
-function ggp 
+function ggp
     git push
 end
 
-function gga 
+function gga
     git add $argv
 end
+
+function fish_user_key_bindings
+    fish_vi_key_bindings  # Ensure Vi mode is enabled
+    bind -M insert \cf forward-char  # Bind Ctrl + F in insert mode
+end
+
 
 
 export EDITOR="nvim"
@@ -52,13 +48,13 @@ export VISUAL="nvim"
 
 # binds
 
-bind \cl forward-char 
+#bind \cf forward-char
 bind \el clear-screen
 
 # pnpm
 set -gx PNPM_HOME "/home/j/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
@@ -66,8 +62,6 @@ end
 
 set -g j_path "$HOME/dev"
 
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /home/j/.lmstudio/bin
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
 
-# Created by `pipx` on 2025-02-18 00:42:11
-set PATH $PATH /home/j/.local/bin
+fish_user_key_bindings
